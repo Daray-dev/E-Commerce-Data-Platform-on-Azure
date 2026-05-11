eed992c4733ae0c5f81fb387cfeebbfd6643e6cd
Daray Molden <moldenray@gmail.com>
2026-05-11 15:35:58 -0500

    Refactor SQL table creation and drop statements

--- Scripts/ddl_bronze_sql.sql
+++ Scripts/ddl_bronze_sql.sql
@@ -1,6 +1,6 @@
-IF OBJECT_ID ('olist_customer_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_customer_dataset;
-CREATE TABLE cust_dataset (
+IF OBJECT_ID ('olist_customer', 'u') IS NOT NULL
+   DROP TABLE olist_customer;
+CREATE TABLE olist_customer (
 customer_id NVARCHAR(100) PRIMARY KEY,
 unique_id NVARCHAR(100),
 cust_zipcod INT,
@@ -9,17 +9,17 @@ Cust_state NVARCHAR(100)
 
 );
 
-IF OBJECT_ID ('olist_geolaction_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_geolaction_dataset;
-CREATE TABLE olist_geolaction_dataset (
+IF OBJECT_ID ('olist_geolaction', 'u') IS NOT NULL
+   DROP TABLE olist_geolaction;
+CREATE TABLE olist_geolaction(
 geo_zip DECIMAL(18,8),
 geo_lat DECIMAL(18, 8),
 geo_city NVARCHAR(100),
 geo_state NVARCHAR(10)
 )
-IF OBJECT_ID ('olist_order_items_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_order_items_dataset;
-CREATE TABLE olist_order_items_dataset ( 
+IF OBJECT_ID ('olist_order_items', 'u') IS NOT NULL
+   DROP TABLE olist_order_items;
+CREATE TABLE olist_order_items ( 
 order_id NVARCHAR(100) PRIMARY KEY,
 ORDER_ITEM_ID INT,
 product_id NVARCHAR(100),
@@ -30,9 +30,9 @@ freight_value INT
 
 );
 
-IF OBJECT_ID ('olist_order_payment_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_order_payment_dataset;
-CREATE TABLE olist_order_payment_dataset (
+IF OBJECT_ID ('olist_order_payment', 'u') IS NOT NULL
+   DROP TABLE olist_order_payment;
+CREATE TABLE olist_order_payment (
 order_id NVARCHAR(100) PRIMARY KEY,
 Payment_sequential INT,
 payment_type NVARCHAR(30),
@@ -41,9 +41,9 @@ payment_value INT
 
 )
 
-IF OBJECT_ID ('olist_order_reviews_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_order_reviews_dataset;
-CREATE TABLE olist_order_reviews_dataset (
+IF OBJECT_ID ('olist_order_reviews', 'u') IS NOT NULL
+   DROP TABLE olist_order_reviews;
+CREATE TABLE olist_order_reviews (
 Review_id NVARCHAR(100) PRIMARY KEY,
 order_id NVARCHAR(100),
 review_score INT,
@@ -54,9 +54,9 @@ review_answer_timestamp DATETIME
 
 );
 
-IF OBJECT_ID ('olist_orders_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_orders_dataset;
-CREATE TABLE olist_orders_dataset (
+IF OBJECT_ID ('olist_orders', 'u') IS NOT NULL
+   DROP TABLE olist_orders;
+CREATE TABLE olist_orders (
   order_id NVARCHAR(50) PRIMARY KEY,
     customer_id NVARCHAR(50),
     order_status NVARCHAR(20),
@@ -68,9 +68,9 @@ CREATE TABLE olist_orders_dataset (
 
 );
 
-IF OBJECT_ID ('olist_products_dataset', 'u') IS NOT NULL
-   DROP TABLE olist_products_dataset;
-CREATE TABLE olist_products_dataset (
+IF OBJECT_ID ('olist_products', 'u') IS NOT NULL
+   DROP TABLE olist_products;
+CREATE TABLE olist_products (
     product_id NVARCHAR(50) PRIMARY KEY,
     product_category_name NVARCHAR(100),
     product_name_lenght INT,
@@ -81,15 +81,15 @@ CREATE TABLE olist_products_dataset (
     product_height_cm INT,
     product_width_cm INT
 );
-IF OBJECT_ID('olist_sellers_dataset','u') IS NOT NULL
-   DROP TABLE olist_sellers_dataset;
-CREATE TABLE olist_sellers_dataset (
+IF OBJECT_ID('olist_sellerst','u') IS NOT NULL
+   DROP TABLE olist_sellers;
+CREATE TABLE olit_sellers (
     seller_id VARCHAR(50) PRIMARY KEY,
     seller_zip_code_prefix INT,
     seller_city VARCHAR(100),
     seller_state CHAR(2)
 );
-CREATE TABLE product_category_translation (
+CREATE TABLE prod_cat_translation (
     product_category_name VARCHAR(100) PRIMARY KEY,
     product_category_name_english VARCHAR(100)
 );
